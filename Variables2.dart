bb6077875c272ba6f363b22f7f67faa7ba3d3002
Min <decemberkms@gmail.com>
2023-08-16 20:48:45 +0200

    Variable change comments

--- Variables2.dart
+++ Variables2.dart
@@ -1,8 +1,16 @@
 void main() {
-  var name; //dynamic typing - like Python
-  dynamic name2; // same as var
+  var name; //dynamic typing - like Python can only change value maybe it also changes type
+  dynamic name2; // can change type and value
   name = 1;
   print(name);
   name = "apple";
+
   print(name);
+  name2 = "banana";
+  if (name2 is String) {
+    print(name2.length);
+  }
+  if (name is String) {
+    print(name.isNotEmpty);
+  }
 }
