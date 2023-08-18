bde089662e8263ef46bdb0f54a4f347fd4596e33
Min <decemberkms@gmail.com>
2023-08-18 22:11:33 +0200

    Class this. < not a must

--- Class1.dart
+++ Class1.dart
@@ -1,11 +1,16 @@
 class Player {
   final String name = 'min';
   int xp = 100;
+
+  void sayHello() {
+    print("Hi ${this.name}");
+  }
 }
 
 void main() {
   var player1 = Player();
   print(player1.name);
+  player1.sayHello();
   // player1.name = 'pee';
   // print(player1.name);
 }
