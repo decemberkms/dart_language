4d4c4f5599ce01339bba1992dc3325d4e0542f5d
Min <decemberkms@gmail.com>
2023-08-19 14:17:58 +0200

    Class named constructor

---
+++ Class3.dart
@@ -0,0 +1,37 @@
+class Player {
+  String name, team;
+  int xp, age;
+
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+
+  Player.createYounger({required String name, required String team})
+      : this.name = name,
+        this.team = team,
+        this.xp = 0,
+        this.age = 10;
+
+  Player.createbPlayer(String name, int age)
+      : this.name = name,
+        this.age = age,
+        this.team = 'b',
+        this.xp = 50;
+
+  void sayHello() {
+    print("Hi ${this.name}. Xp is ${this.xp}, age is ${this.age}");
+  }
+}
+
+void main() {
+  var player1 = Player(name: 'min', xp: 100, team: 'a', age: 30);
+  var player2 = Player(name: 'min', xp: 100, team: 'a', age: 25);
+  var player3 = Player.createYounger(name: 'min', team: 'a');
+  var player4 = Player.createbPlayer('min', 15);
+  player1.sayHello();
+  player2.sayHello();
+  player3.sayHello();
+  player4.sayHello();
+}
