9934b32aa010a069beda0332148b8ec1823dce6c
Min <decemberkms@gmail.com>
2023-08-19 14:21:05 +0200

    Class named constructor systanic sugar

--- Class3.dart
+++ Class3.dart
@@ -20,6 +20,12 @@ class Player {
         this.team = 'b',
         this.xp = 50;
 
+  Player.createaPlayer(
+      {required this.name,
+      required this.age,
+      this.team = 'a',
+      this.xp = 10000});
+
   void sayHello() {
     print("Hi ${this.name}. Xp is ${this.xp}, age is ${this.age}");
   }
@@ -30,8 +36,10 @@ void main() {
   var player2 = Player(name: 'min', xp: 100, team: 'a', age: 25);
   var player3 = Player.createYounger(name: 'min', team: 'a');
   var player4 = Player.createbPlayer('min', 15);
+  var player5 = Player.createaPlayer(name: 'min', age: 43);
   player1.sayHello();
   player2.sayHello();
   player3.sayHello();
   player4.sayHello();
+  player5.sayHello();
 }
