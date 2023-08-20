0ba89809cc9120d42db868d85621626d94c48bea
Min <decemberkms@gmail.com>
2023-08-20 15:42:18 +0200

    Class inheritance2

--- Class6.dart
+++ Class6.dart
@@ -11,10 +11,18 @@ enum Team { a, b }
 
 class Player extends Human {
   final Team team;
-  Player(super.name, super.age, this.team);
+  Player({required this.team, required String name, required int age})
+      : super(name, age);
+
+  @override
+  void sayHello() {
+    // TODO: implement sayHello
+    super.sayHello();
+    print("new say hello ${this.name}");
+  }
 }
 
 void main() {
-  var player1 = Player('din', 21, Team.a);
+  var player1 = Player(name: 'din', age: 21, team: Team.a);
   player1.sayHello();
 }
